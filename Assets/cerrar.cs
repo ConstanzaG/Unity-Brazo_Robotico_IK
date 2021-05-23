0429137da47df3809dae80f99a9440fe1b2c4602
Constanza González <44975847+ConstanzaG@users.noreply.github.com>
2021-05-22 22:15:08 -0400

    Add files via upload

---
+++ Assets/cerrar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cerrar : MonoBehaviour
+{
+    public Transform cuerpo;
+    public Transform cuerpo1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("m") & cuerpo.localPosition.x < -0.3 )
+        {
+            cuerpo.localPosition += new Vector3(0.01f, 0, 0);
+            cuerpo1.localPosition -= new Vector3(0.01f, 0, 0);
+        }
+        
+        if (Input.GetKey("n") & cuerpo.localPosition.x > -1)
+        {
+            cuerpo1.localPosition += new Vector3(0.01f, 0, 0);
+            cuerpo.localPosition -= new Vector3(0.01f, 0, 0);
+        }
+    }
+}
---
+++ Assets/mov_Eslabon_1_eje.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class mov_Eslabon_1_eje : MonoBehaviour
+{
+    
+    public float speed = 0.1f;
+    public float grados = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 position = transform.position;
+        //print("La posición es: " + position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Girar la base con grados
+        //transform.rotation = Quaternion.Euler(0, default, grados);
+    }
+
+  
+ }
+*/
